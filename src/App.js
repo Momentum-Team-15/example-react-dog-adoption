834156498ccf6c0441f4591c7bcad43a84d9ef7e
Amy Gori <amydgori@gmail.com>
2022-11-03 20:07:39 -0400

    Add styling and disclosure for traits

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import 'bulma/css/bulma.min.css';
-import { useState } from 'react';
+import 'bulma/css/bulma.min.css'
+import { useState } from 'react'
 
 function App({ dogData }) {
   return (
-    <div className="App m-3">
-      <h1 className="title">Dogs Go Here</h1>
-      <div className="dog-list">
-          {dogData.map(dog =>
-            <DogCard dogName={dog.name} />
-          )}
-      </div>
-
+    <div className="App">
+      <header className="has-background-info-dark p-5 mb-3">
+        <h1 className="title has-text-white">Happy Valley Shelter</h1>
+      </header>
+      <h2 className="is-size-4 ml-5">Adopt a Dog</h2>
+      <section className="dog-list is-flex is-flex-wrap-wrap mx-3">
+        {dogData.map((dog) => (
+          <DogCard
+            dogName={dog.name}
+            image={dog.image}
+            description={dog.description}
+            traits={dog.traits}
+          />
+        ))}
+      </section>
     </div>
-  );
+  )
 }
 
-
-const DogCard = ({dogName}) => {
+const DogCard = ({ dogName, image, description, traits }) => {
   const [expanded, setExpanded] = useState(false)
 
   const handleClick = (event) => {
@@ -24,14 +30,38 @@ const DogCard = ({dogName}) => {
   }
 
   return (
-    <div className="dog-card">
-      <h2>{dogName}</h2>
-      <button className="button" onClick={handleClick}>Show more info</button>
-      { expanded &&
-        <div className="dog-description">This is a nice dog</div>
-      }
-
+    <div
+      className="dog-card card m-3 mt-6"
+      style={{ maxWidth: '300px', height: '400px' }}
+    >
+      <div className="card-image">
+        <figure class="image is2by3">
+          <img src={image} alt={description} />
+        </figure>
+      </div>
+      <div className="p-3">
+        <div className="is-flex is-justify-content-space-between">
+          <h2 className="subtitle mb-0 has-text-weight-semibold">{dogName}</h2>
+          <button
+            className="ml-2 button is-small is-outlined"
+            onClick={handleClick}
+          >
+            {expanded ? 'Less' : 'More'} Info
+          </button>
+        </div>
+        {expanded && (
+          <div className="dog-traits" style={{ maxHeight: '300px' }}>
+            <ul className="is-flex is-flex-wrap-wrap">
+              {traits.map((trait) => (
+                <li className="p-1">
+                  <span className="tag is-info is-light">{trait}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
-export default App;
+export default App
